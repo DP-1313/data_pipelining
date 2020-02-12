39949d95865a4419e664df0f5259dadb2a5357f3
Lim Tae Hyun <dlatns0201@naver.com>
2020-02-12 18:16:16 +0900

    feature: _app.js/ 고차 컴포넌트를 사용한 html head 작성
    antd를 위한 css link 추가
    title 설정

--- frontEnd/src/pages/_app.js
+++ frontEnd/src/pages/_app.js
@@ -1,10 +1,21 @@
-const AppLayout = ({ Component }) => {
+import Head from "next/head";
+import AppLayout from "../components/AppLayout";
+
+const App = ({ Component }) => {
   return (
-    <div>
-      <h1> AppLayout </h1>
-      <Component />
-    </div>
+    <>
+      <Head>
+        <title> Data Pipelining </title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.9/antd.min.css"
+        />
+      </Head>
+      <AppLayout>
+        <Component />
+      </AppLayout>
+    </>
   );
 };
 
-export default AppLayout;
+export default App;
