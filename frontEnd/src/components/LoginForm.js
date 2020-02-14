3473809140c326a2e9febf9f20ae9f537b51122f
Lim Tae Hyun <dlatns0201@naver.com>
2020-02-14 21:18:43 +0900

    feat: components_login, signup / 로그인, 회원가입 폼 뷰 작성

--- frontEnd/src/components/LoginForm.js
+++ frontEnd/src/components/LoginForm.js
@@ -1,5 +1,40 @@
+import { Form, Input, Button } from "antd";
+import styled from "styled-components";
+
+const StyledLoginForm = styled.div`
+  input.ant-input {
+    display: inline-block;
+    opacity: 0.7;
+    line-height: 2rem;
+    height: initial;
+  }
+  form.ant-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > * {
+      margin: 0.8rem 0;
+    }
+
+    button[type="submit"] {
+      transform: scale(1.3);
+    }
+  }
+`;
 const LoginForm = () => {
-  return <div>Login Form</div>;
+  return (
+    <StyledLoginForm>
+      <Form>
+        <Input type="text" placeholder="ID" />
+        <Input type="password" placeholder="Password" />
+        <Button htmlType="submit" ghost>
+          {" "}
+          Submit{" "}
+        </Button>
+      </Form>
+    </StyledLoginForm>
+  );
 };
 
 export default LoginForm;
--- frontEnd/src/components/SignupForm.js
+++ frontEnd/src/components/SignupForm.js
@@ -1,5 +1,43 @@
+import { Form, Input, Button } from "antd";
+import styled from "styled-components";
+
+const StyledSignupForm = styled.div`
+  input.ant-input {
+    display: inline-block;
+    opacity: 0.7;
+    line-height: 2rem;
+    height: initial;
+  }
+  form.ant-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & > * {
+      margin: 0.8rem 0;
+    }
+
+    button[type="submit"] {
+      transform: scale(1.3);
+    }
+  }
+`;
+
 const SignupForm = () => {
-  return <div>signup form</div>;
+  return (
+    <StyledSignupForm>
+      <Form>
+        <Input type="email" name="userId" placeholder="Email" />
+        <Input type="password" name="password" placeholder="Password" />
+        <Input type="password" placeholder="Password Check" />
+        <Input type="text" name="nickname" placeholder="Nickname" />
+        <Button htmlType="submit" ghost>
+          {" "}
+          Submit{" "}
+        </Button>
+      </Form>
+    </StyledSignupForm>
+  );
 };
 
 export default SignupForm;
